5e2d65f6a32ee342454ea98959c1344962974064
joyadel36 <ja1193@fayoum.edu.eg>
2023-12-31 00:26:45 +0200

    details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BookDetailsComponentComponent } from './components/book-details-component/book-details-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet ,BookDetailsComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
