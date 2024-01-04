de582a126649354d53d49266a241d6882a18e603
islam <im9988077@gmail.com>
2024-01-04 22:19:38 +0200

    first version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BookViewComponent } from './Components/book-view/book-view.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { BookDetailsComponentComponent } from './Components/book-details-component/book-details-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'' , component:BookViewComponent   },
+    {path: 'about_us' , component:AboutUsComponent },
+    {path:'details' , component:BookDetailsComponentComponent}
+];
--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -20,7 +20,7 @@ import { register } from 'swiper/element/bundle';
 export class AboutUsComponent implements AfterViewInit {
   swiperData = [
     {
-      image: '../../../assets/images/image1.jpg',
+      image: '../../../assets/Images/image1.jpg',
       title: 'Sohila Tarek',
       text: 'Full-Stack .Net Developer',
       details:
@@ -28,28 +28,28 @@ export class AboutUsComponent implements AfterViewInit {
       details2: 'Year: 2023',
     },
     {
-      image: '../../../assets/images/image2.jpg',
+      image: '../../../assets/Images/image2.jpg',
       title: 'Joy Adel',
       text: 'Full-Stack .Net Developer',
       details: 'Graduated From: Computers and artificial intelligence',
       details2: 'Year: 2023',
     },
     {
-      image: '../../../assets/images/image3.jpg',
+      image: '../../../assets/Images/image3.jpg',
       title: 'Islam Mohamed',
       text: 'Full-Stack .Net Developer',
       details: 'Graduated From: Faculty Of Engineering Cairo University',
       details2: 'Year: 2023',
     },
     {
-      image: '../../../assets/images/image4.jpg',
+      image: '../../../assets/Images/image4.jpg',
       title: 'Mina Bahgt',
       text: 'Full-Stack .Net Developer',
       details: 'Graduated From: Faculty of computer and information systems',
       details2: 'Year: 2020',
     },
     {
-      image: '../../../assets/images/image5.jpg',
+      image: '../../../assets/Images/image5.jpg',
       title: 'Fady Yousry',
       text: 'Full-Stack .Net Developer',
       details: 'Graduated From: Faculty of computer and information systems',
