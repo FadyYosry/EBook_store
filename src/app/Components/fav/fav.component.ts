663aa67b468784cae39aa9ac8a8143082519820c
sohilatarek <NOTE BOOK@DESKTOP-9EDJTVD>
2024-01-11 22:50:05 +0200

    added fav and updated add-to-cart

--- src/app/Components/fav/fav.component.ts
+++ src/app/Components/fav/fav.component.ts
@@ -18,65 +18,39 @@ interface Book {
   numOfBookNeed?: number;
 }
 @Component({
-  selector: 'app-add-to-cart',
+  selector: 'app-fav',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './add-to-cart.component.html',
-  styleUrl: './add-to-cart.component.css',
+  templateUrl: './fav.component.html',
+  styleUrl: './fav.component.css',
 })
-export class AddToCartComponent {
+export class FavComponent {
   cartItems: Book[] = [
     {
       title: 'Book 1',
       price: '10',
-      imagesmallThumbnail: 'url_image_1',
+      imagesmallThumbnail: 'url_to_image_1',
       numOfBookNeed: 1,
     },
     {
       title: 'Book 2',
       price: '15',
-      imagesmallThumbnail: 'url_image_2',
+      imagesmallThumbnail: 'url_to_image_2',
       numOfBookNeed: 2,
     },
     {
       title: 'Book 3',
       price: '20',
-      imagesmallThumbnail: 'url_image_3',
+      imagesmallThumbnail: 'url_to_image_3',
       numOfBookNeed: 3,
     },
     {
       title: 'Book 4',
       price: '25',
-      imagesmallThumbnail: 'url_image_4',
+      imagesmallThumbnail: 'url_to_image_4',
       numOfBookNeed: 4,
     },
   ];
-
-  addToCart(book: Book): void {
-    const existingBook = this.cartItems.find(
-      (item) => item.title === book.title
-    );
-
-    if (existingBook) {
-      existingBook.numOfBookNeed = (existingBook.numOfBookNeed ?? 0) + 1;
-    } else {
-      this.cartItems.push({ ...book, numOfBookNeed: 1 });
-    }
-  }
-
-  removeFromCart(book: Book): void {
-    const existingBook = this.cartItems.find(
-      (item) => item.title === book.title
-    );
-
-    if (existingBook) {
-      existingBook.numOfBookNeed = (existingBook.numOfBookNeed ?? 0) - 1;
-
-      if (existingBook.numOfBookNeed <= 0) {
-        this.removeItem(book);
-      }
-    }
-  }
 
   removeItem(book: Book): void {
     this.cartItems = this.cartItems.filter((item) => item.title !== book.title);
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { ApplicationModule, Component } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { AllBooksService } from '../../Services/all_books/allBooks.service';
 import { CartService } from '../../Services/cart/cart.service';
 import { FavService } from '../../Services/fav/fav.service';
@@ -10,47 +10,55 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule , CommonModule , FormsModule , ApplicationModule  ],
+  imports: [RouterModule, CommonModule, FormsModule, ApplicationModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  allbooks:any
-  filterdbooks1:any
-  searchTerm:any
-  constructor(private fire_store:Firestore,private bookurl:ActivatedRoute,private serve:AllBooksService,private cart:CartService,private fav:FavService){
-}
+  allbooks: any;
+  filterdbooks1: any;
+  searchTerm: any;
+  constructor(
+    private fire_store: Firestore,
+    private bookurl: ActivatedRoute,
+    private serve: AllBooksService,
+    private cart: CartService,
+    private fav: FavService,
+    private router: Router
+  ) {}
 
-ngOnInit(): void {
-  this.serve.getAllFromAllBooks().subscribe(data =>{this.allbooks=data})
-}
+  ngOnInit(): void {
+    this.serve.getAllFromAllBooks().subscribe((data) => {
+      this.allbooks = data;
+    });
+  }
+  goToAddToCart() {
+    this.router.navigate(['/add-to-cart']);
+  }
+  goToAddToFav() {
+    this.router.navigate(['/add-to-fav']);
+  }
 
-fire(){
- 
-}
-remove(){
-  this.filterdbooks1=[];
-}
-sendData1(event :any){
-  this.filterdbooks1=[];
+  fire() {}
+  remove() {
+    this.filterdbooks1 = [];
+  }
+  sendData1(event: any) {
+    this.filterdbooks1 = [];
 
-       this.searchTerm=event.target.value;
-       for (let book of this.allbooks){
-        if (this.searchTerm.trim()==book.title){
-          this.filterdbooks1.push(book.title)
+    this.searchTerm = event.target.value;
+    for (let book of this.allbooks) {
+      if (this.searchTerm.trim() == book.title) {
+        this.filterdbooks1.push(book.title);
+      }
+      for (let i = 0; i <= book.title.length; i++) {
+        if (
+          this.searchTerm.trim() == book.title.slice(0, i + 1).toLowerCase() ||
+          this.searchTerm.trim() == book.title.slice(0, i + 1).toUpperCase()
+        ) {
+          this.filterdbooks1.push(book);
         }
-        for(let i=0 ; i<=book.title.length ; i++){
-         
-          if (this.searchTerm.trim()==book.title.slice(0,i+1).toLowerCase()||this.searchTerm.trim()==book.title.slice(0,i+1).toUpperCase()){
-          this.filterdbooks1.push(book)
-          }
-          
-        }
-   
-  
+      }
     }
-
-
-}
-
+  }
 }
