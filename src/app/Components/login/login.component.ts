2b8fe4f213560e67255c31bdffe92ea4daf7ff68
sohilatarek <NOTE BOOK@DESKTOP-9EDJTVD>
2024-01-15 23:04:36 +0200

    login with validation

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,17 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   @ViewChild('passwordInput') passwordInput!: ElementRef;
 
+  loginForm: FormGroup;
   loginEmail: string = '';
   loginPassword: string = '';
   rememberMe: boolean = false;
@@ -22,7 +31,28 @@ export class LoginComponent {
   signupPassword: string = '';
   currentForm: 'login' | 'signup' = 'login';
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      loginEmail: ['', [Validators.required, Validators.email]],
+      loginPassword: [
+        '',
+        [Validators.required, Validators.minLength(5), this.passwordValidator],
+      ],
+    });
+  }
+
+  passwordValidator(control: AbstractControl) {
+    if (control.value) {
+      const hasUpperCase = /[A-Z]/.test(control.value);
+      const hasLowerCase = /[a-z]/.test(control.value);
+
+      const valid = hasUpperCase && hasLowerCase;
+
+      return valid ? null : { invalidPassword: true };
+    }
+
+    return null;
+  }
 
   //show password func
   togglePasswordVisibility() {
@@ -36,4 +66,16 @@ export class LoginComponent {
       this.renderer.setProperty(passwordInput, 'type', 'password');
     }
   }
+
+  submitLoginForm() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('loginEmail')?.value;
+      const password = this.loginForm.get('loginPassword')?.value;
+
+      console.log('Email:', email);
+      console.log('Password:', password);
+    }
+  }
+
+  submitSignUpForm() {}
 }
